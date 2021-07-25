0759c900a32b686906b5b803fc3f6ddc56eadadc
rzlamrr <rzlamrr.dvst@protonmail.com>
2021-07-25 03:06:22 +0000

    Add extras for xdroid_lavender-userdebug 11 RKQ1.210701.002 eng.xyzuan.20210723.000644 test-keys
    Signed-off-by: rzlamrr <rzlamrr.dvst@protonmail.com>

---
+++ README.md
@@ -0,0 +1,18 @@
+## xdroid_lavender-userdebug 11 RKQ1.210701.002 eng.xyzuan.20210723.000644 test-keys
+- Manufacturer: xiaomi
+- Platform: sdm660
+- Codename: lavender
+- Brand: Xiaomi
+- Flavor: xdroid_lavender-userdebug
+- Release Version: 11
+- Id: RKQ1.210701.002
+- Incremental: eng.xyzuan.20210723.000644
+- Tags: test-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
+- OTA version: 
+- Branch: xdroid_lavender-userdebug-11-RKQ1.210701.002-eng.xyzuan.20210723.000644-test-keys
+- Repo: xiaomi_lavender_dump
---
+++ join_split_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cat system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/product/priv-app/Velvet/Velvet.apk
+rm -f system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/product/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/product/priv-app/DevicePersonalizationPrebuiltPixel2020/DevicePersonalizationPrebuiltPixel2020.apk.* 2>/dev/null >> system/system/product/priv-app/DevicePersonalizationPrebuiltPixel2020/DevicePersonalizationPrebuiltPixel2020.apk
+rm -f system/system/product/priv-app/DevicePersonalizationPrebuiltPixel2020/DevicePersonalizationPrebuiltPixel2020.apk.* 2>/dev/null
+cat system/system/product/app/Messages/Messages.apk.* 2>/dev/null >> system/system/product/app/Messages/Messages.apk
+rm -f system/system/product/app/Messages/Messages.apk.* 2>/dev/null
+cat system/system/product/app/WallpapersBReel2020/WallpapersBReel2020.apk.* 2>/dev/null >> system/system/product/app/WallpapersBReel2020/WallpapersBReel2020.apk
+rm -f system/system/product/app/WallpapersBReel2020/WallpapersBReel2020.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat system/system/product/app/Photos/Photos.apk.* 2>/dev/null >> system/system/product/app/Photos/Photos.apk
+rm -f system/system/product/app/Photos/Photos.apk.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat bootimg/01_dtbdump_Qualcomm_Technologies,_Inc._SDM_660_PM660_+_PM660L_MTP.dtb.* 2>/dev/null >> bootimg/01_dtbdump_Qualcomm_Technologies,_Inc._SDM_660_PM660_+_PM660L_MTP.dtb
+rm -f bootimg/01_dtbdump_Qualcomm_Technologies,_Inc._SDM_660_PM660_+_PM660L_MTP.dtb.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat vendor/apex/com.android.vndk.current.on_vendor.apex.* 2>/dev/null >> vendor/apex/com.android.vndk.current.on_vendor.apex
+rm -f vendor/apex/com.android.vndk.current.on_vendor.apex.* 2>/dev/null
